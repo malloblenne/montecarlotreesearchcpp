fb1d59912124738ad0fb24294bff0ec1af121efa
Mauro <malloblenne@gmail.com>
2022-09-04 18:21:09 +0200

    simpleselectionpolicy: improve api

--- src/simpleselectionpolicy.h
+++ src/simpleselectionpolicy.h
@@ -8,11 +8,11 @@
 
 namespace mctsearch
 {
-    class SimpleSelectionPolicy
+    class SimpleSelectionPolicy: public SelectionPolicy
     {
         public:
         SimpleSelectionPolicy(std::unique_ptr<ExpansionPolicy> e, std::unique_ptr<BestChild> b);
-        TreeSearchNode* select_new(TreeSearchNode* node) const;
+        TreeSearchNode* select_new(TreeSearchNode* node) const override;
 
         private:
         std::unique_ptr<ExpansionPolicy> expansion;
