2377e1c0108cd3b38b1aa5d2fda9b42ee73f0af5
Mauro <malloblenne@gmail.com>
2022-09-04 18:20:02 +0200

    expansionpolicy: improve api

--- src/expansionpolicy.h
+++ src/expansionpolicy.h
@@ -1,10 +1,12 @@
 #pragma once
-
+#include "treesearchnode.h"
 
 namespace mctsearch
 {
     class ExpansionPolicy
     {
-
+        public:
+        virtual ~ExpansionPolicy() = default;
+        virtual TreeSearchNode* expand(TreeSearchNode* node) = 0;
     };
 }
