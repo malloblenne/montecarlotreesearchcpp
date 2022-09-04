6a1460b2a36c91daf1ff385d978f595644c9ddcf
Mauro <malloblenne@gmail.com>
2022-09-04 18:16:46 +0200

    Improve abstract back propagation policy

--- src/backpropagationpolicy.h
+++ src/backpropagationpolicy.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include "treesearchnode.h"
+#include "gamestate.h"
+
 
 namespace mctsearch
 {
     class BackPropagationPolicy
     {
+        public:
 
+        virtual ~BackPropagationPolicy() = default;
+
+        virtual void update(TreeSearchNode* node, WinningState reward) = 0;
     };
 }
