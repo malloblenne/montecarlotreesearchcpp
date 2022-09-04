24f1304d0089b85c42f5d366438e9831c36f5c0e
Mauro <malloblenne@gmail.com>
2022-09-04 18:19:47 +0200

    bestchild: improve api

--- src/bestchild.h
+++ src/bestchild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "treesearchnode.h"
-
+#include "action.h"
 
 
 namespace mctsearch
@@ -9,6 +9,9 @@ namespace mctsearch
     class BestChild
     {
         public:
-        TreeSearchNode* choose(TreeSearchNode* node) const;
+        virtual ~BestChild() = default;
+        virtual TreeSearchNode* choose(TreeSearchNode* node) const = 0;
+        virtual std::unique_ptr<Action> choose_action(TreeSearchNode* node) const = 0;
+        virtual std::unique_ptr<Action> choose_action_exploitation_only(TreeSearchNode* node) const = 0;
     };
 }
