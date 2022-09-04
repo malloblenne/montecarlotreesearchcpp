357a61f37e06c8335865425e501a44bebfa70dc1
Mauro Brenna <mbrenna@lely.com>
2022-09-04 18:16:07 +0200

    Improve abstract selection policy

--- src/selectionpolicy.h
+++ src/selectionpolicy.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include "treesearchnode.h"
+
+
 
 namespace mctsearch
 {
     class SelectionPolicy
     {
+        public:
+        virtual ~SelectionPolicy() = default;
+
+        virtual TreeSearchNode* select_new(TreeSearchNode* node) const = 0;
 
     };
 }
