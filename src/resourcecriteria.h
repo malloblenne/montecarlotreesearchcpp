5c0f732f9b702a58eb947b147589882eafddab17
Mauro Brenna <mbrenna@lely.com>
2022-09-04 18:17:12 +0200

    Improve abstract resource criteria

--- src/resourcecriteria.h
+++ src/resourcecriteria.h
@@ -6,6 +6,8 @@ namespace mctsearch
     class ResourceCriteria
     {
         public:
+        virtual ~ResourceCriteria() = default;
+        
         virtual std::string to_string() const = 0;
         virtual void init() = 0;
         virtual bool expired() const = 0;
