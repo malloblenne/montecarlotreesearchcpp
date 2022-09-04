9d6ebe9e040be567b9c1febda9aaabe7a88af2e1
Mauro Brenna <mbrenna@lely.com>
2022-09-04 18:15:35 +0200

    action: Improve abstract action class

--- src/action.h
+++ src/action.h
@@ -6,6 +6,7 @@ namespace mctsearch
     class Action
     {
         public:
+        virtual ~Action() = default;
         virtual std::string to_string() const = 0;
         virtual int value() const = 0;
     };
