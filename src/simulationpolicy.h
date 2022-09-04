9a4f4b6af3e7e487dbb9fb13b8e571f66aba4401
Mauro <malloblenne@gmail.com>
2022-09-04 18:16:21 +0200

    Improve abstract simulation policy

--- src/simulationpolicy.h
+++ src/simulationpolicy.h
@@ -1,10 +1,13 @@
 #pragma once
-
+#include "gamestate.h"
 
 namespace mctsearch
 {
     class SimulationPolicy
     {
+        public:
+        virtual ~SimulationPolicy() = default;
 
+        virtual WinningState simulate(const GameState* state) = 0;
     };
 }
